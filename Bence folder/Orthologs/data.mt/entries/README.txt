3e9f4ba70ec2eee8c29c19f813eab0eea9c0f34b
BKover99 <91386576+BKover99@users.noreply.github.com>
2022-11-18 16:20:06 +0000

    nov 18. updated analysis and new data

---
+++ Bence folder/Orthologs/data.mt/entries/README.txt
@@ -0,0 +1,3 @@
+This folder comprises a Hail (www.hail.is) native Table or MatrixTable.
+  Written with version 0.2.104-1940d9e8eaab
+  Created at 2022/11/01 17:09:17
--- Bence folder/QTL mapping/QTL washing.R
+++ Bence folder/QTL mapping/QTL washing.R
@@ -1,10 +1,16 @@
-setwd("/Users/celestecohen/Downloads")
-d<-read.table(file = 'SupplementaryDataset_S7_genotype.tsv', sep = '\t', header = TRUE)
+#install.packages("/Users/bencekover/Downloads/RFQTL.tar.gz", repos = NULL)
 library(RFQTL)
-genotype1<-d[,-c(1:4)]
+#install.packages("randomForest")
+library(randomForest)
+
+library(qqman)
+#loading in the genotype presence-absence matrix
+d<-read.table(file = '/Users/bencekover/Library/CloudStorage/OneDrive-UniversityCollegeLondon/MSci Bahler lab/S.-Pombe-biofilm/Bence folder/QTL mapping/SupplementaryDataset_S7_genotype.tsv', sep = '\t', header = TRUE)
+genotype1<-d[,-c(1:4)]vi
 genotype1<-data.matrix(genotype1)
 genotype1<-t(genotype1)
-phenotype1<-read.csv('Biofilm_bioinformatics/Biofilm_image_processing/washing_phenotypes.csv')
+#loading in the washing_phenotype measurements
+phenotype1<-read.csv("/Users/bencekover/Library/CloudStorage/OneDrive-UniversityCollegeLondon/MSci Bahler lab/S.-Pombe-biofilm/Bence folder/QTL mapping/washing_phenotypes.csv")
 phenotype1<-phenotype1[,-1]
 phenotype1<-phenotype1[which(phenotype1$strain %in% rownames(genotype1)),]
 strainNames1 <- phenotype1$strain
@@ -16,40 +22,33 @@ mappingData1 <- preMap(genotype=genotype1,
                       phenotype=phenotype1,
                       sampleInfo=sampleInfo1,
                       scale=T)
-library(randomForest)
+
 
 #Get accurate realscores by calculating them 20x and averaging them
-RS1=c()
-for(i in 1:20){
-  r=rfMapper(mappingData = mappingData1,
+
+r=rfMapper(mappingData = mappingData1,
              permute = F,
-             nforest = 100,
-             ntree = 150)
-  RS1=c(RS1,r)
-}
-RS1_df=matrix(RS1, ncol=20, byrow=F)
-RS1_df=data.frame(RS1_df)
-realScores1<-rowMeans(RS1_df)
+             nforest = 100,#was 100
+             ntree = 100)
 
 #Get accurate p-values by doing 3500 permutations (100x35)
 #Here the wd is a folder in which your permutations will go
-setwd("/Users/celestecohen/Downloads/Biofilm_bioinformatics/washing_permutations/permutations")
-for(i in 1:35){
-  permutedScores1 <- rfMapper(mappingData = mappingData1,
-                              permute = T,
-                              nforest = 100,
-                              ntree = 150,
-                              nPermutations=100,
-                              file=paste("wash_permut",i,".RData",sep=""),
-                              nCl=4,
-                              clType="SOCK")
-}
+setwd("/Users/bencekover/Library/CloudStorage/OneDrive-UniversityCollegeLondon/MSci Bahler lab/S.-Pombe-biofilm/Bence folder/QTL mapping/permutations/permutations")
+
+permutedScores1 <- rfMapper(mappingData = mappingData1,
+                            permute = T,
+                            nforest = 100,#was 100
+                            ntree = 100,
+                            nPermutations=10000, #was 100
+                            file="wash_permut.RData",
+                            nCl=6,
+                            clType="SOCK")
 
 #Below the wd is the folder in which the permutations folder is (not IN the permutation folder)
-setwd("/Users/celestecohen/Downloads/Biofilm_bioinformatics")
+setwd("/Users/bencekover/Library/CloudStorage/OneDrive-UniversityCollegeLondon/MSci Bahler lab/S.-Pombe-biofilm/Bence folder/QTL mapping/permutations")
 #The path below is the permutations folder
-pValues1 <- pEst(path="washing_permutations/permutations/",
-                scores=realScores1,
+pValues1 <- pEst(path="permutations/",
+                scores=r,
                 markersPerIteration = 350,
                 printProg = T,
                 pCorrection = "none")
@@ -58,18 +57,19 @@ pValuesX1 <- pValues1[mappingData1$genotype2group]
 #These steps (until markerPositions) are not useful for the way I presented the results
 chrVec1 <- d$chromosome
 QTL_list1 <- QTLgrouper(pmat = pValuesX1,
-                       sigThreshold = 0.1,
-                       corThreshold = 0.8,
-                       distThreshold = 9,
+                       sigThreshold = 0.01,
+                       corThreshold = 0.99,
+                       distThreshold = 1,
                        genotype = genotype1,
                        chrVec = chrVec1)
 
-#this is useful
+#Plotting the marker positions
 markerPositions1 <- d[,c(1,2)]
 markerPositions1$chromosome<-gsub("chromosome_1",1,markerPositions1$chromosome)
 markerPositions1$chromosome<-gsub("chromosome_2",2,markerPositions1$chromosome)
 markerPositions1$chromosome<-gsub("chromosome_3",3,markerPositions1$chromosome)
 markerPositions1[,3]=markerPositions1$position
+
 
 #the writeQTL is what is in the tutorial but I didn't find it useful as a visualisation of results
 writeQTL(QTLlist = QTL_list1,traitNames = "Flocc",markerPositions = markerPositions1,path="myResults1.qtl")
@@ -77,28 +77,38 @@ qtl1 <- readQTL(path = "myResults1.qtl")
 qtl1
 
 
+
+
 #Presenting results
 #barplot
 barplot(-log10(pValuesX1))
-abline(h=-log10(0.01),col="red")
+abline(h=-log10(0.05/length(pValues1)),col="red")
+
 
 #manhattan plot
 results.tab=markerPositions1[,-3]
 results.tab[,3]=pValuesX1
 colnames(results.tab)=c("CHR","BP","P")
-library(qqman)
+
 results.tab=results.tab[-which(is.na(results.tab)),]
 results.tab$CHR=as.numeric(unlist(results.tab$CHR))
 results.tab[,4]=rep("snp",length(results.tab$CHR))
 colnames(results.tab)[4]="SNP"
-manhattan(results.tab,ylim = c(0,4), suggestiveline = F, genomewideline = F,cex = 0.7)
-abline(h=-log10(0.01),col="red")
-text(x=1,y=-log10(0.01)+0.1,labels="       p=0.01",col="red",cex=0.7)
+manhattan(results.tab,ylim = c(0,7), suggestiveline = F, genomewideline = F,cex = 0.7)
+abline(h=-log10(0.05/length(pValues1)),col="red")
+text(x=1,y=-log10(0.05/length(pValues1))+0.1,labels="       p=0.00001",col="red",cex=0.7)
 
 #from the following, determine the regions to use for reg_map below
-markerPositions1[which(-log10(pValuesX1)>=2),] 
+snps = markerPositions1[which(-log10(pValuesX1)>=2.5),] 
+results = d[row.names(snps),]
+write.csv(results,"results.csv")
+plot(genotype1[,qtl1[[1]]$mostSignificantPredictor], phenotype1)
+plot(genotype1[,qtl1[[2]]$mostSignificantPredictor], phenotype1)
 
 
+d[qtl1[[2]]$predictors,]
+
+#manually
 #mapping regions on chromosomes
 chromosomes=c("chr1","chr2","chr3")
 start=c(1,1,1)
